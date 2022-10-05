a2c3d28e7f28790c361eadf237c9fe3b30516584
EL_SADEK <81380165+Ahmed-Sadek89@users.noreply.github.com>
2022-10-05 20:57:21 +0200

    9 search algorithm binary search recursion (#8)
    * 8-search-algorithm-binary-search
    
    * 9-search-algorithm-binary-search-recursion

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 function binary_search(key, array) {
-    let leftIndex = 0;
-    let rightIndex = array.length - 1;
-    while( leftIndex <= rightIndex ) {
-        let middle = Math.floor((leftIndex + rightIndex) / 2);
-        if( key === array[middle] ){
-            return middle
-        } else if ( key <= array[middle] ) {
-            rightIndex = middle - 1
-        } else {
-            leftIndex = middle + 1
-        }
-    }
-    return -1
+    return search(key, array, 0, array.length - 1);
 }
 
-console.log(binary_search(2, [1,2,3,4])) // if search from one side of array => O(log(n))
-// else => O(n)
+function search(key, array, left, right) {
+    if(left > right) {
+        return -1 // O(1)
+    }
+    let middle = Math.floor((left + right) / 2);
+
+    if( key === array[middle] ) {
+        return middle; // O(1)
+    } else if ( key < array[middle] ) {
+        return search(key, array, left, middle - 1) // O(logn)
+    } else {
+        return search(key, array, middle + 1, right)// O(logn)
+    }
+}
+
+console.log(binary_search(2, [1,2,3,4])) //  search from one side of array => O(log(n))
