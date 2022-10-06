fe0f28c1acf48e660290b3b5d57947be3f01d920
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2022-10-06 19:07:43 +0200

    10-sorting-algorithm-bubble-sort

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
-function binary_search(key, array) {
-    return search(key, array, 0, array.length - 1);
+function bubble_sort(array) {
+    // key =>  for swapping between two index
+    // numberOfSwapping =>  counting the swapping number
+    // numberOfLooping => for counting the number of loops in the array, 
+
+    let key = 0, numberOfLooping = 0, numberOfSwapping;
+
+    while( numberOfSwapping !== 0 ) { // n
+
+        numberOfSwapping = 0; // firstly, this must be 0.
+        numberOfLooping++ //if is 1, that mean the elements are already sorted sorted. 
+
+        for( let i = 0; i < array.length; i++ ) { // n
+            if(array[i] > array[i+1] ) {
+                key = array[i];
+                array[i] = array[i+1];
+                array[i+1] = key;
+                numberOfSwapping++ // increase it, if array[i] > array[i+1]
+            } else {
+                continue;
+            }
+        }
+        console.log(numberOfLooping)
+    }
+
+    return array
 }
 
-function search(key, array, left, right) {
-    if(left > right) {
-        return -1 // O(1)
-    }
-    let middle = Math.floor((left + right) / 2);
-
-    if( key === array[middle] ) {
-        return middle; // O(1)
-    } else if ( key < array[middle] ) {
-        return search(key, array, left, middle - 1) // O(logn)
-    } else {
-        return search(key, array, middle + 1, right)// O(logn)
-    }
-}
-
-console.log(binary_search(2, [1,2,3,4])) //  search from one side of array => O(log(n))
+console.log(bubble_sort([3,1,2,-10,4])) // O(n^2)
