329a4cf691457f37c2a3533e93178d7eff41ace4
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2022-10-08 16:41:01 +0200

    11-sorting-algorithms-insertion-sort

--- main.js
+++ main.js
@@ -1,29 +1,19 @@
-function bubble_sort(array) {
-    // key =>  for swapping between two index
-    // numberOfSwapping =>  counting the swapping number
-    // numberOfLooping => for counting the number of loops in the array, 
-
-    let key = 0, numberOfLooping = 0, numberOfSwapping;
-
-    while( numberOfSwapping !== 0 ) { // n
-
-        numberOfSwapping = 0; // firstly, this must be 0.
-        numberOfLooping++ //if is 1, that mean the elements are already sorted sorted. 
-
-        for( let i = 0; i < array.length; i++ ) { // n
-            if(array[i] > array[i+1] ) {
-                key = array[i];
-                array[i] = array[i+1];
-                array[i+1] = key;
-                numberOfSwapping++ // increase it, if array[i] > array[i+1]
-            } else {
-                continue;
-            }
+function insertion_sort(array) {
+    let key;
+    for( let i = 1; i < array.length; i++ ) {
+        key = array[i]
+        j = i - 1;
+        while( j > -1 && array[j] > key ) { // case if array[j] > key
+            array[j + 1] = array[j];
+            j--
         }
-        console.log(numberOfLooping)
+        array[j + 1] = key // case if array[j] !> key
     }
 
-    return array
+    return array;
 }
 
-console.log(bubble_sort([3,1,2,-10,4])) // O(n^2)
+console.log(insertion_sort([-6, 20, 8, -2, 4]))
+
+// best case O(n)
+// worst case O(n^2) => array is sorted desc
